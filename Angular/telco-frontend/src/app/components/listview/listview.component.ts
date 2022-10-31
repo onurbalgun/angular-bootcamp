00818e3453bc36cee3701d23d0bbb6fac6e7c6a5
onurbalgun <onur.balgun@gmail.com>
2022-10-31 17:01:13 +0300

    29.10.2022 Ödevi

--- Angular/telco-frontend/src/app/components/listview/listview.component.ts
+++ Angular/telco-frontend/src/app/components/listview/listview.component.ts
@@ -1,23 +1,139 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+
+import { CategoriesService } from './../../services/categories.service';
+import { Category } from 'src/app/models/category';
 
 @Component({
   selector: 'app-listview',
   templateUrl: './listview.component.html',
-  styleUrls: ['./listview.component.css']
+  styleUrls: ['./listview.component.css'],
 })
 export class ListviewComponent implements OnInit {
+  // component içerisinde yer alan properties bizim için state oluyor.
+  // ?: null olabilir demek.
+  // !: null olmayacak, bu property'i kullanmadan önce atama işlemini gerçekleştiriceğiz söz vermiş oluyoruz.
+  categories!: Category[];
+  language: string = 'en';
 
+  categoryAddForm!: FormGroup;
 
-  categories:string[]=[];
+  categoryIdToDelete: number = 0; // state
+  categoryIdToUpdate: number = 0;
 
-  constructor() { }
+  error: string = '';
+  updateForm: boolean = false;
+
+  //Angular IoC (Inversion of Control) Container kullanır.
+  //Dependency Injection (Bağımlılık Enjeksiyonu)
+  constructor(
+    private categoriesService: CategoriesService,
+    private formBuilder: FormBuilder
+  ) {}
 
   ngOnInit(): void {
-    this.getCategories()
-  }
-  getCategories()
-  {
-    this.categories=["Gi","Ayakkabı","Parfüm"]
+    this.getCategories();
+    this.createCategoryAddForm();
   }
 
+  createCategoryAddForm() {
+    this.categoryAddForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      description: ['', [Validators.required, Validators.minLength(10)]],
+    });
+  }
+
+  getCategories(): void {
+    // Object tipi henüz belli olmayan referans tip diyebiliriz. Referans tiplerin en temel sınıfı diyebiliriz.
+    this.categoriesService.getCategories().subscribe((response) => {
+      // Observer Design Pattern
+      this.categories = response;
+    });
+  }
+
+  // changecategoryIdToDelete(event: any) {
+  //   this.categoryIdToDelete = event.target.value;
+  // }
+
+  add(): void {
+    if (this.categoryAddForm.invalid) {
+      this.error = 'Form is invalid';
+      return;
+    }
+    if (this.error) this.error = '';
+
+    // const {name, description} = this.categoryAddForm.value;
+    // // this.categoryAddForm.value
+    // const category: Category = {
+    //   id: 0,
+    //   // name: name,
+    //   name,
+    //   description,
+    // };
+
+    // spread operator ... (ES6)
+    const category: Category = {
+      ...this.categoryAddForm.value,
+    };
+    if (this.updateForm) {
+      category.id = this.categoryIdToUpdate;
+      this.categoriesService.update(category).subscribe({
+        next: (response) => {
+          console.info(`Category(${response.id}) has been  updated.`);
+        },
+        error: (err) => {
+          console.log(err);
+
+          this.error = err.statusText;
+        },
+        complete: () => {
+          this.updateForm = false;
+
+          this.categoryAddForm.reset();
+          this.getCategories();
+        },
+      });
+      return;
+    }
+    this.categoriesService.add(category).subscribe({
+      next: (response) => {
+        console.info(`Category(${response.id}) has added.`);
+      },
+      error: (err) => {
+        console.log(err);
+
+        this.error = err.statusText;
+      },
+      complete: () => {
+        if (this.error) this.error = '';
+        this.categoryAddForm.reset();
+        this.getCategories();
+      },
+    });
+  }
+
+  delete() {
+    this.categoriesService.delete(this.categoryIdToDelete).subscribe(() => {
+      this.categoryIdToDelete = 0;
+      this.getCategories();
+    });
+  }
+  edit(index: number) {
+    this.categoryIdToUpdate = index + 1;
+    this.updateForm = true;
+    this.categoriesService
+      .getCategory(this.categoryIdToUpdate)
+      .subscribe((response) => {
+        console.log(response);
+        this.categoryAddForm.setValue({
+          name: response.name,
+          description: response.description,
+        });
+      });
+  }
 }
